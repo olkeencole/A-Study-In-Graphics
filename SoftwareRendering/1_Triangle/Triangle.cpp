1e04604cba15301bd72b0992718dc94777963ce6
olkeencole <keenancole@gmail.com>
2017-10-20 21:36:29 -0500

    Added BoxDemo in Software R.

--- SoftwareRendering/1_Triangle/Triangle.cpp
+++ SoftwareRendering/1_Triangle/Triangle.cpp
@@ -7,7 +7,7 @@ $Notice:
 
 /* A Study In Graphics
    API: Software Rendering
-   Demo: Simple Triangle With Color Interpolation
+   Demo: Perspective Correct Rendering of a bunch of Boxes
 
    - As simple as a script can be initializing the Windows API and using
    it to render a simple triangle from some vertices
@@ -65,210 +65,7 @@ float verts[] = {
 }; 
 
 
-// Shaders
-// Just displays pos in NDC and colors
-const char* vertexShader = 
-	"#version 330 core\n"
-	"layout (location = 0) in vec3 verts;\n" 
-	"layout (location = 1) in vec3 vColor;\n"
-	"out vec3 color;\n" 
-
-	"void main() { \n"
-		"gl_Position = vec4(verts.x, verts.y, verts.z, 1.0);\n"
-		"color = vColor;\n"
-	"}\n\0" ;
-
-const char* fragmentShader = 
-	"in vec3 color;\n"
-	"void main(){\n"
-		"gl_FragColor = vec4(color, 1.0);\n" 
-	"}\n\0";
-
-
 Vector3 positionOffset; 
-
-//WGL CONSTANTS FLAGS
-//
-#define WGL_CONTEXT_MAJOR_VERSION_ARB           	0x2091
-#define WGL_CONTEXT_MINOR_VERSION_ARB           	0x2092
-#define WGL_CONTEXT_LAYER_PLANE_ARB             	0x2093
-#define WGL_CONTEXT_FLAGS_ARB                   	0x2094
-#define WGL_CONTEXT_PROFILE_MASK_ARB            	0x9126
-#define WGL_CONTEXT_DEBUG_BIT_ARB               	0x0001
-#define WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB     0x0002
-#define WGL_CONTEXT_CORE_PROFILE_BIT_ARB           0x00000001
-#define WGL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB  0x00000002
-#define ERROR_INVALID_VERSION_ARB                  0x2095
-#define ERROR_INVALID_PROFILE_ARB                  0x2096
-
-
-
-//OPENGL CONSTANT FLAGS
-	//
-#define GL_ARRAY_BUFFER                   0x8892
-#define GL_STATIC_DRAW                    0x88E4
-
-#define GL_VERTEX_SHADER                  0x8B31
-#define GL_FRAGMENT_SHADER                0x8B30
-
-#define GL_COMPILE_STATUS                 0x8B81
-#define GL_LINK_STATUS                    0x8B82
-
-
- 
-// OPENGL FUNCTION POINTER TYPE DEFINITIONS
-//	
-typedef ptrdiff_t GLsizeiptr;
-typedef char GLchar;
-
-typedef HGLRC WINAPI wgl_create_attrib_arb(HDC hdc, HGLRC sharedContext, const int *attribs );
-
-typedef void WINAPI gl_GenVertexArrays (GLsizei n, GLuint *arrays);
-typedef void WINAPI gl_GenBuffers (GLsizei n, GLuint *buffers);
-typedef void WINAPI gl_BindVertexArray (GLuint array);
-typedef void WINAPI gl_BindBuffer (GLenum target, GLuint buffer);
-typedef void WINAPI gl_BufferData (GLenum target, GLsizeiptr size, const void *data, GLenum usage);
-typedef void WINAPI gl_VertexAttribPointer (GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const void *pointer);
-typedef void WINAPI gl_EnableVertexAttribArray (GLuint index);
-
-typedef void   WINAPI gl_CompileShader (GLuint shader);
-typedef GLuint WINAPI gl_CreateProgram (void);
-typedef GLuint WINAPI gl_CreateShader  (GLenum type);
-typedef void   WINAPI gl_DeleteShader  (GLuint shader);
-typedef void   WINAPI gl_ShaderSource  (GLuint shader, GLsizei count, const GLchar *const *string, const GLint *length);
-typedef void   WINAPI gl_AttachShader  (GLuint program, GLuint shader);
-typedef void   WINAPI gl_LinkProgram   (GLuint program);
-typedef void   WINAPI gl_UseProgram    (GLuint program);
-
-typedef void  WINAPI gl_GetProgramiv      (GLuint program, GLenum pname, GLint *params);
-typedef void  WINAPI gl_GetProgramInfoLog (GLuint program, GLsizei bufSize, GLsizei *length, GLchar *infoLog);
-typedef void  WINAPI gl_GetShaderiv       (GLuint shader, GLenum pname, GLint *params);
-typedef void  WINAPI gl_GetShaderInfoLog  (GLuint shader, GLsizei bufSize, GLsizei *length, GLchar *infoLog);
-
-
-
-// FUNCTION POINTER DECLARATIONS
-//
-gl_GenVertexArrays  		*glGenVertexArrays;
-gl_BindVertexArray  		*glBindVertexArray; 
-gl_GenBuffers       		*glGenBuffers; 
-gl_BindBuffer       		*glBindBuffer; 
-gl_BufferData       		*glBufferData; 
-gl_VertexAttribPointer		*glVertexAttribPointer; 
-gl_EnableVertexAttribArray  *glEnableVertexAttribArray;
-
-gl_AttachShader   *glAttachShader ;  
-gl_CompileShader  *glCompileShader;  
-gl_CreateProgram  *glCreateProgram;  
-gl_CreateShader   *glCreateShader ;  
-gl_DeleteShader   *glDeleteShader ;  
-gl_ShaderSource   *glShaderSource ;  
-gl_LinkProgram    *glLinkProgram  ;  
-gl_UseProgram     *glUseProgram   ;  
-
-gl_GetProgramiv       *glGetProgramiv      ;
-gl_GetProgramInfoLog  *glGetProgramInfoLog ;
-gl_GetShaderiv        *glGetShaderiv       ;
-gl_GetShaderInfoLog   *glGetShaderInfoLog  ;
-
-
-
-/// GLOBALS
-//
-static HGLRC GlobalRenderContext; 
-static int WindowWidth  = 800; 
-static int WindowHeight = 600; 
-static GLuint VAO,VBO, Shader; 
-
-
-// Store Vertex Data In Buffer In GPU
-//
-void InitOpenGL()
-{
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts,  GL_STATIC_DRAW ); //6
-
-	int index = 6; 
-	glVertexAttribPointer(0 , 3,  GL_FLOAT, GL_FALSE, sizeof(float) * index ,   (void *) 0); //6 
-	glEnableVertexAttribArray(0);
-
-	glVertexAttribPointer(1 , 3,  GL_FLOAT, GL_FALSE, sizeof(float) * index ,   (void *) ( 3 * sizeof(float) ) ); //6 
-	glEnableVertexAttribArray(1);
-
-	glBindVertexArray(0);
-
- } 
-
-
-
-// Compile Shader and Store in GPU Buffer
-//
- void InitShader()
- {
- 	//Debuging
- 	char log[512]; 
- 	int success;
- 	GLuint vertex, frag, shader; 
-
- 	vertex = glCreateShader(GL_VERTEX_SHADER); 
- 	glShaderSource(vertex, 1, &vertexShader, NULL); //4 id, count, shader text, null < length
- 	glCompileShader( vertex);// just takes the idea
-	glGetShaderiv( vertex, GL_COMPILE_STATUS, &success);
-	 
- 	//if(!success)
- 	{
- 		glGetShaderInfoLog(vertex, 512, NULL, log); // //shader, log limit, dnull, charlog)
- 		std::cout << "SHADER ERROR: " << log << std::endl;
- 	}
-
- 	frag = glCreateShader(GL_FRAGMENT_SHADER);
- 	glShaderSource( frag, 1, &fragmentShader, NULL);
- 	glCompileShader(frag); 
-
- 	shader = glCreateProgram();
- 	glAttachShader(shader, vertex);
- 	glAttachShader(shader, frag);
- 	glLinkProgram(shader); 
-	glGetProgramiv(shader, GL_LINK_STATUS, &success );
- 	
- 	//if( !success)
- 	{
- 		glGetProgramInfoLog( shader, 512, NULL, log );
- 		std::cout << "LINKING ERROR: " << log << std::endl;
- 	}
-
- 	glDeleteShader(vertex); 
- 	glDeleteShader(frag);  
-
- 	Shader = shader;
- }
-
-
-// Draw with OpenGLCore
-//
- void RenderOpenGLContext(GLuint shader)
- {
- 	glUseProgram(shader); 
-
- 	glBindVertexArray(VAO);
- 	glDrawArrays( GL_TRIANGLES, 0 , 3 );
-
- 	glBindVertexArray(0);
- }
-
-
-
-global_variable BITMAPINFO bitmapInfo; 
-global_variable void *BitmapMemory; 
-global_variable HBITMAP bitmapHandle; 
-global_variable HDC BitmapDeviceContext;
-global_variable int bitmapWidth; 
-global_variable int bitmapHeight; 
-global_variable int BytesPersPixel = 4; 
 
 
 void DrawPixel(uint32 color, int x, int y)
@@ -278,6 +75,7 @@ void DrawPixel(uint32 color, int x, int y)
 	uint32 *pixel = (uint32 *)  base + (uint32)(x + y * (bitmapWidth)  ) ;
 	*pixel = color;
 }
+
 
 uint32 GetColor( int r, int g, int b)
 {
@@ -653,6 +451,7 @@ void DrawBaryTriangle(   const Vector3 &v1, const Vector3 &v2, const Vector3 &v3
 
 }
 
+
 void DrawBaryTriangle(   const Vector3 &v1, const Vector3 &v2, const Vector3 &v3, Vector3 &color1, Vector3 &color2, Vector3 &color3){
 
 	// Compute Bounding Box
@@ -709,7 +508,6 @@ void DrawBaryTriangle(   const Vector3 &v1, const Vector3 &v2, const Vector3 &v3
 }
  
  Mesh model; 
-
 
  Vector3 Convert2(Vec3f v){
  	Vector3 result = Vector3(v.x, v.y, v.z);
@@ -769,12 +567,7 @@ void DrawBaryTriangle(   const Vector3 &v1, const Vector3 &v2, const Vector3 &v3
     }
 
     uint32 color = GetColor(vertices[realCount-3], vertices[realCount-2], vertices[realCount-1] ); 
-   // DrawLineNDC(color,  vertices[realCount-6], vertices[realCount-5],  vertices[0], vertices[1]   );
-
-	//DrawTriangle( triangle[2], triangle[0],  triangle[1]);
-	// FillFlatBottomTriangle( ConvertNDCToScreen( triangle[2] ), ConvertNDCToScreen( triangle[0]),  ConvertNDCToScreen( triangle[1] )) ;
-	// FillFlatTopTriangle( ConvertNDCToScreen( flatTop[0] ), ConvertNDCToScreen( flatTop[1]),  ConvertNDCToScreen( flatTop[2] )) ;
-   
+ 
 
     //Draw last Vert to First Vert
 
---
+++ SoftwareRendering/2_Box/build.bat
@@ -0,0 +1,8 @@
+@echo off
+
+if "%1" == "1" call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+
+mkdir build
+pushd build
+cl  -FC -Zi /I include  ..\BoxDemo.cpp -W2 -wd4312 user32.lib  gdi32.lib kernel32.lib opengl32.lib 
+popd
